d46459757cd8015fdf764e8a35c7afb0455e7c38
Jean-Roch Masson <jyaire@gmail.com>
2020-07-14 17:42:43 +0200

    Configure sass and bootstrap

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,9 +6,20 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import '../css/app.css';
+import '../css/app.scss';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
-// import $ from 'jquery';
+const $ = require('jquery');
+// this "modifies" the jquery module: adding behavior to it
+// the bootstrap module doesn't export/return anything
+require('bootstrap');
+
+// or you can include specific pieces
+// require('bootstrap/js/dist/tooltip');
+// require('bootstrap/js/dist/popover');
+
+$(document).ready(function() {
+    $('[data-toggle="popover"]').popover();
+});
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
