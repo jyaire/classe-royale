bf6fddcd203a5d9e2c129b42be036bad08a70221
Jean-Roch Masson <jyaire@gmail.com>
2020-07-19 20:45:15 +0200

    Add flash message on base with JS for closing button

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,14 +5,13 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
+require('bootstrap');
+
 // any CSS you import will output into a single css file (app.css in this case)
-import '../css/app.scss';
+require('../css/app.scss');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 const $ = require('jquery');
-// this "modifies" the jquery module: adding behavior to it
-// the bootstrap module doesn't export/return anything
-require('bootstrap');
 
 // or you can include specific pieces
 // require('bootstrap/js/dist/tooltip');
