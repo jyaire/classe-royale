5a1119d9b4596ca9be7b6c0ca06824a200d01240
Jean-Roch Masson <jyaire@gmail.com>
2020-07-26 12:20:27 +0200

    Make User crud

---
+++ src/Form/UserType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email')
+            ->add('roles')
+            ->add('password')
+            ->add('lastname')
+            ->add('firstname')
+            ->add('isWoman')
+            ->add('dateCreate')
+            ->add('dateModif')
+            ->add('isVerified')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
