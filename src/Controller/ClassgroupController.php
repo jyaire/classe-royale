93dba7c0a9efad263890e1964724d4f6730eabab
Jean-Roch Masson <jyaire@gmail.com>
2020-08-02 18:22:50 +0200

    Classgroup CRUD

---
+++ src/Controller/ClassgroupController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Classgroup;
+use App\Form\ClassgroupType;
+use App\Repository\ClassgroupRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/classgroup")
+ */
+class ClassgroupController extends AbstractController
+{
+    /**
+     * @Route("/", name="classgroup_index", methods={"GET"})
+     */
+    public function index(ClassgroupRepository $classgroupRepository): Response
+    {
+        return $this->render('classgroup/index.html.twig', [
+            'classgroups' => $classgroupRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="classgroup_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $classgroup = new Classgroup();
+        $form = $this->createForm(ClassgroupType::class, $classgroup);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($classgroup);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('classgroup_index');
+        }
+
+        return $this->render('classgroup/new.html.twig', [
+            'classgroup' => $classgroup,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="classgroup_show", methods={"GET"})
+     */
+    public function show(Classgroup $classgroup): Response
+    {
+        return $this->render('classgroup/show.html.twig', [
+            'classgroup' => $classgroup,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="classgroup_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Classgroup $classgroup): Response
+    {
+        $form = $this->createForm(ClassgroupType::class, $classgroup);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('classgroup_index');
+        }
+
+        return $this->render('classgroup/edit.html.twig', [
+            'classgroup' => $classgroup,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="classgroup_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Classgroup $classgroup): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$classgroup->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($classgroup);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('classgroup_index');
+    }
+}
---
+++ src/Form/ClassgroupType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Classgroup;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ClassgroupType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('ref')
+            ->add('school')
+            ->add('teacher')
+            ->add('section')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Classgroup::class,
+        ]);
+    }
+}
