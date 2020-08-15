76e0bbcfd00bbc4e0000cfccf090ad4a0b837713
Jean-Roch Masson <jyaire@gmail.com>
2020-08-15 16:47:29 +0200

    Admin can see list of schools

--- src/Controller/SchoolController.php
+++ src/Controller/SchoolController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("director/school")
+ * @Route("school")
  */
 class SchoolController extends AbstractController
 {
