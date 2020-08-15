3a211a4b7519a2906f7eec4562dbfbb139dcafb3
Jean-Roch Masson <jyaire@gmail.com>
2020-08-15 20:52:18 +0200

    Make crud for Subject and Card

---
+++ src/Controller/CardController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Card;
+use App\Form\CardType;
+use App\Repository\CardRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/card")
+ */
+class CardController extends AbstractController
+{
+    /**
+     * @Route("/", name="card_index", methods={"GET"})
+     */
+    public function index(CardRepository $cardRepository): Response
+    {
+        return $this->render('card/index.html.twig', [
+            'cards' => $cardRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="card_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $card = new Card();
+        $form = $this->createForm(CardType::class, $card);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($card);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('card_index');
+        }
+
+        return $this->render('card/new.html.twig', [
+            'card' => $card,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="card_show", methods={"GET"})
+     */
+    public function show(Card $card): Response
+    {
+        return $this->render('card/show.html.twig', [
+            'card' => $card,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="card_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Card $card): Response
+    {
+        $form = $this->createForm(CardType::class, $card);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('card_index');
+        }
+
+        return $this->render('card/edit.html.twig', [
+            'card' => $card,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="card_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Card $card): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$card->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($card);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('card_index');
+    }
+}
---
+++ src/Form/CardType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Card;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CardType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('description')
+            ->add('level')
+            ->add('type')
+            ->add('image')
+            ->add('subject')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Card::class,
+        ]);
+    }
+}
