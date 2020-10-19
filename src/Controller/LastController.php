b2a524bc79b91f21cdf46efef78ec98549c0d16a
Jean-Roch Masson <jyaire@gmail.com>
2020-10-19 11:05:01 +0200

    Last points for a student on student/id/last

--- src/Controller/LastController.php
+++ src/Controller/LastController.php
@@ -3,8 +3,8 @@
 namespace App\Controller;
 
 use App\Repository\PointRepository;
+use App\Repository\StudentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -19,6 +19,42 @@ class LastController extends AbstractController
     {
         return $this->render('point/index.html.twig', [
             'points' => $pointRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/{user}/{id}/last", name="last_custom", methods={"GET"})
+     * @param PointRepository $pointRepository
+     * @param StudentRepository $studentRepository
+     * @return Response
+     */
+    public function custom(
+        PointRepository $pointRepository, 
+        StudentRepository $studentRepository, 
+        string $user, 
+        int $id
+        ): Response
+    {
+        if($user == 'student') {
+            $points = $pointRepository->findBy(['student'=>$id]);
+        }
+        elseif($user == 'classgroup') {
+            /* to be debugged
+            $points = [];
+            $students = $studentRepository->findBy(['classgroup'=>$id]);
+            foreach($students as $student) {
+                $points = $pointRepository->findBy(['student'=>$student]);
+                array_push($points, $points);
+            }
+            */
+            $points = $pointRepository->findBy(['student'=>$id]);
+        }
+        else {
+            $points = $pointRepository->findAll();
+        }
+        
+        return $this->render('point/index.html.twig', [
+            'points' => $points,
         ]);
     }
 }
