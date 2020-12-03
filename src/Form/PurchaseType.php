f5081fbc69ec58d4fdd196d06d6fd3fd2626c0f1
Jean-Roch Masson <jyaire@gmail.com>
2020-12-03 14:09:16 +0100

    CRUD for Purchase

---
+++ src/Form/PurchaseType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Purchase;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PurchaseType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('description')
+            ->add('price')
+            ->add('currency')
+            ->add('avatar')
+            ->add('student')
+            ->add('classgroup')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Purchase::class,
+        ]);
+    }
+}
