37f900234c90f64ff7a0495d9ce0c51067f2186c
Jean-Roch Masson <jyaire@gmail.com>
2020-12-05 23:10:58 +0100

    When a student pay a purchase, new point is set and price is remove from student points

--- src/Controller/PurchaseController.php
+++ src/Controller/PurchaseController.php
@@ -2,10 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Point;
 use App\Entity\Purchase;
+use App\Entity\Reason;
 use App\Entity\Student;
 use App\Form\PurchaseType;
 use App\Repository\PurchaseRepository;
+use App\Repository\ReasonRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\Request;
@@ -106,10 +109,45 @@ class PurchaseController extends AbstractController
      * @Route("/confirm/{id}/{student}", name="purchase_confirm", methods={"GET"})
      * @param Purchase $purchase
      * @param Student $student
+     * @param ReasonRepository $reasonRepository
      */
-    public function confirm(Purchase $purchase, Student $student): Response
+    public function confirm(Purchase $purchase, Student $student, ReasonRepository $reasonRepository): Response
     {
+        $entityManager = $this->getDoctrine()->getManager();
+        // add purchase to student
+        $student->addPurchase($purchase);
+        // add XP and remove price (gold or elixir)
+        $student->setXp($student->getXp()+5);
+        if ($purchase->getCurrency() == "gold") {
+            $student->setGold($student->getGold()-$purchase->getPrice());
+        }
+        else {
+            $student->setElixir($student->getElixir()-$purchase->getPrice());
+        }
+        // add point and reason
+        $point = new Point;
+        $sentence = 'Achat de "' . $purchase->getName() . '"';
+        // search if reason already exists
+        $search = $reasonRepository->findOneBy(['sentence'=>$sentence]);
+        if(!empty($search)) {
+            $point->setReason($search);
+        } else {
+            $reason = new Reason;
+            $reason->setSentence($sentence);
+            $entityManager->persist($reason);
+            $point->setReason($reason);
+        }
+        $point
+            ->setStudent($student)
+            ->setQuantity(-$purchase->getPrice())
+            ->setType($purchase->getCurrency())
+            ->setDate(new \DateTime())
+            ->setAuthor($this->getUser());
         
+        $entityManager->persist($student);
+        $entityManager->persist($point);
+        $entityManager->flush();
+
         $message = $student->getFirstname() . ' a acheté "' . $purchase->getName() . '"';
         $this->addFlash('success', $message);
         return $this->redirectToRoute('student_show', [
