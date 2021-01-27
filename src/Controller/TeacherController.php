714f0361a80d1beef787e3b444f86a4a2d94c1e8
Jean-Roch Masson <jyaire@gmail.com>
2021-01-27 22:10:42 +0100

    Add form for invitated teacher

--- src/Controller/TeacherController.php
+++ src/Controller/TeacherController.php
@@ -3,9 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\Classgroup;
+use App\Repository\ClassgroupRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class TeacherController extends AbstractController
 {
@@ -17,6 +21,62 @@ class TeacherController extends AbstractController
     {
     
         return $this->render('teacher/index.html.twig');
+    }
+
+    /**
+     * @Route("/invite", name="invite_teacher")
+     * @IsGranted("ROLE_USER")
+     * @param Request $request
+     * @param StudentRepository $studentRepository
+     * @return RedirectResponse|Response
+     */
+    public function invite(Request $request, ClassgroupRepository $classgroupRepository, AuthorizationCheckerInterface $authChecker)
+    {
+        $form = $this->createFormBuilder()
+            ->add('invit', TextType::class, [
+                'label' => "Code enseignant reçu par mail",
+            ])
+            ->getForm();
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // add teacher to classgroup if code is ok
+
+            $entityManager = $this->getDoctrine()->getManager();
+
+            $classgroup = $classgroupRepository->findOneBy([
+                'invit' => $form->getData()['invit'],
+                ]);
+            if(empty($classgroup)) {
+                $this->addFlash('danger', 'Ce code est invalide');
+                return $this->redirectToRoute('invite_teacher');
+            } else {
+                $classgroup->addTeacher($this->getUser());
+                $entityManager->persist($classgroup);
+            }
+
+            // add role_teacher to user if not already teacher
+            if (!$authChecker->isGranted('ROLE_TEACHER')) {
+                $user = $this->getUser();
+                $roles = $user->getRoles();
+                array_push($roles, "ROLE_TEACHER");
+                $user->setRoles($roles);
+                $entityManager->persist($user);
+                $message = 'Vous êtes devenu "enseignant", il faut vous reconnecter';
+                $this->addFlash('danger', $message);
+            }
+
+            $entityManager->flush();
+
+            $message = 'Vous êtes enseignant dans la classe "' . $classgroup->getName() . '"';
+            $this->addFlash('success', $message);
+            return $this->redirectToRoute('teacher');
+        }
+
+        return $this->render('teacher/invite.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 
     /**
